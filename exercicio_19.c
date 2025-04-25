e224ed37e5e4dc838aee3c81fc5488165854e199
guizeuvirs0919 <guilherme.reis3@estudante.ifms.edu.br>
2025-04-24 21:18:04 -0400

    exercicio_19.c

--- exercicio_19.c
+++ exercicio_19.c
@@ -1,22 +1,24 @@
+//algoritimo que pede 2 numeros reais e depois multiplica eles e diz se deu um numero inteiro ou nao 
+
 #include <stdio.h>
 
 int main() {
     float num1, num2;
     int resultado;
 
-    printf("Digite o primeiro número real: ");
+    printf("Digite o primeiro numero real: ");
     scanf("%f", &num1);
 
-    printf("Digite o segundo número real: ");
+    printf("Digite o segundo numero real: ");
     scanf("%f", &num2);
 
     float multiplicacao = num1 * num2;
 
     if (multiplicacao == (int)multiplicacao) {
         resultado = (int)multiplicacao;
-        printf("A multiplicaçao dos numeros e: %d\n", resultado);
+        printf("A multiplicaÃ§ao dos numeros e: %d\n", resultado);
     } else {
-        printf("A multiplicaçao nao resulta em um número inteiro.\n");
+        printf("A multiplicaÃ§ao nao resulta em um numero inteiro.\n");
     }
 
     return 0;
