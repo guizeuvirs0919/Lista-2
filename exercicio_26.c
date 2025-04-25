be963125e4c46135d5a6c86035b59be20405b3a4
guizeuvirs0919 <guilherme.reis3@estudante.ifms.edu.br>
2025-04-24 21:45:08 -0400

    exercicio_26.c

--- exercicio_26.c
+++ exercicio_26.c
@@ -1,20 +1,22 @@
+//algoritimo que pega 2 numeros digitado, soma eles e fala se Ã© ou nao maior que 10
+
 #include <stdio.h>
 
 int main() {
     int num1, num2, soma;
 
-    printf("Digite o primeiro número inteiro: ");
+    printf("Digite o primeiro numero inteiro: ");
     scanf("%d", &num1);
 
-    printf("Digite o segundo número inteiro: ");
+    printf("Digite o segundo numero inteiro: ");
     scanf("%d", &num2);
 
     soma = num1 + num2;
 
     if (soma >= 10) {
-        printf("A soma é %d. Resultado: Maior ou igual a 10.\n", soma);
+        printf("A soma Ã© %d. Resultado: Maior ou igual a 10.\n", soma);
     } else {
-        printf("A soma é %d. Resultado: Menor que 10.\n", soma);
+        printf("A soma Ã© %d. Resultado: Menor que 10.\n", soma);
     }
 
     return 0;
