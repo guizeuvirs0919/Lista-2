544f561fa0b5d6781f11d6a69bc93e026f5bcb84
guizeuvirs0919 <guilherme.reis3@estudante.ifms.edu.br>
2025-04-24 21:47:41 -0400

    exercicio_25.c

--- exercicio_25.c
+++ exercicio_25.c
@@ -5,14 +5,14 @@
 int main() {
     int numeros[6];
 
-    printf("Digite 6 números para o sorteio:\n");
+    printf("Digite 6 numeros para o sorteio:\n");
 
     for (int i = 0; i < 6; i++) {
         printf("Número %d: ", i + 1);
         scanf("%d", &numeros[i]);
     }
 
-    printf("\nNúmeros escolhidos para o sorteio:\n");
+    printf("\nNumeros escolhidos para o sorteio:\n");
     for (int i = 0; i < 6; i++) {
         printf("%d ", numeros[i]);
     }
