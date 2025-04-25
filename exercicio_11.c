3097f2b03c5225df0c86251f081b3c79cd25848b
guizeuvirs0919 <guilherme.reis3@estudante.ifms.edu.br>
2025-04-24 20:36:53 -0400

    exercicio_11.c

--- exercicio_11.c
+++ exercicio_11.c
@@ -1,23 +1,25 @@
+//algorito que pede um numerador e um divisor para realizar uma diviz√£o, dando erro quando o usuario coloca 0 ou quando nao da um numero inteiro
+
 #include <stdio.h>
 
 int main() {
     int num1, num2;
 
-    printf("Digite o primeiro numero (dividendo): ");
+    printf("Digite o primeiro numero (numerador): ");
     scanf("%d", &num1);
 
     printf("Digite o segundo numero (divisor): ");
     scanf("%d", &num2);
 
     if (num2 == 0) {
-        printf("Erro: divisao por zero n„o È permitida.\n");
+        printf("Erro: divisao por zero n√£o √© permitida.\n");
     } else {
 
         if (num1 % num2 == 0) {
             int resultado = num1 / num2;
             printf("Resultado da divisao exata: %d\n", resultado);
         } else {
-            printf("A divisao de %d por %d n„o e exata (n„o resulta em numero inteiro).\n", num1, num2);
+            printf("A divisao de %d por %d n√£o e exata (n√£o resulta em numero inteiro).\n", num1, num2);
         }
     }
 
