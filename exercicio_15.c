9ada096b5d0917a13f735e77ffd32ca18518e490
guizeuvirs0919 <guilherme.reis3@estudante.ifms.edu.br>
2025-04-24 20:49:51 -0400

    exercicio_15.c

--- exercicio_15.c
+++ exercicio_15.c
@@ -1,3 +1,5 @@
+//algoritimo que fala pro usuario digitar 3 numeros e que no final divide os 3 para ter a mÃ©dia
+
 #include <stdio.h>
 
 int main() {
@@ -5,21 +7,21 @@ int main() {
     int soma;
     float media;
 
-    printf("Digite o primeiro número: ");
+    printf("Digite o primeiro nÃºmero: ");
     scanf("%d", &num1);
 
-    printf("Digite o segundo número: ");
+    printf("Digite o segundo nÃºmero: ");
     scanf("%d", &num2);
 
-    printf("Digite o terceiro número: ");
+    printf("Digite o terceiro nÃºmero: ");
     scanf("%d", &num3);
 
     soma = num1 + num2 + num3;
 
-    media = soma / 3.0;  // Usamos 3.0 para garantir que a média seja calculada como um número decimal
+    media = soma / 3.0; 
 
-    printf("A soma dos números é: %d\n", soma);
-    printf("A média dos números é: %.2f\n", media);
+    printf("A soma dos nÃºmeros Ã©: %d\n", soma);
+    printf("A mÃ©dia dos nÃºmeros Ã©: %.2f\n", media);
 
     return 0;
 }
