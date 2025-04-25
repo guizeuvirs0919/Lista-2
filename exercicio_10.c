2d118c30351b392581223525283e0a19f30bf78b
guizeuvirs0919 <guilherme.reis3@estudante.ifms.edu.br>
2025-04-24 20:33:59 -0400

    exercicio_10.c

--- exercicio_10.c
+++ exercicio_10.c
@@ -1,4 +1,4 @@
-//algoritimo que faz um cardapio e que o usuario pode escolher as opções 
+//algoritimo que faz um cardapio e que o usuario pode escolher as opções 1, 2, 3, 4 e 5
 
 #include <stdio.h>
 
@@ -6,10 +6,10 @@ int main() {
     int opcao;
 
     printf("===== CARDÁPIO =====\n");
-    printf("1 - Hamburguer\n");
-    printf("2 - Pizza\n");
+    printf("1 - Bomba de nitrogenio\n");
+    printf("2 - Arroz com feijao em baixo\n");
     printf("3 - Salada\n");
-    printf("4 - Refrigerante\n");
+    printf("4 - Guarana\n");
     printf("5 - Sorvete\n");
     printf("====================\n");
 
@@ -17,17 +17,17 @@ int main() {
     scanf("%d", &opcao);
 
     if (opcao == 1) {
-        printf("Voce escolheu: Hamburguer\n");
+        printf("Voce escolheu: Bomba de nitrogenio\n");
     } else if (opcao == 2) {
-        printf("Voce escolheu: Pizza\n");
+        printf("Voce escolheu: Arroz com feijao em baixo\n");
     } else if (opcao == 3) {
         printf("Voce escolheu: Salada\n");
     } else if (opcao == 4) {
-        printf("Voce escolheu: Refrigerante\n");
+        printf("Voce escolheu: Guarana\n");
     } else if (opcao == 5) {
         printf("Voce escolheu: Sorvete\n");
     } else {
-        printf("Opção inválida. Por favor, escolha um número de 1 a 5.\n");
+        printf("Nao temos isso. Por favor, escolha um número de 1 a 5.\n");
     }
 
     return 0;
