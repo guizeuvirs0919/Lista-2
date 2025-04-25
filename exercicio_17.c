da179e226eeb2a79c9208f19b1ba0373c316636e
guizeuvirs0919 <guilherme.reis3@estudante.ifms.edu.br>
2025-04-24 21:10:13 -0400

    exercicio_17.c

--- exercicio_17.c
+++ exercicio_17.c
@@ -1,19 +1,20 @@
+//algoritimo que pega 5 numeros inteiros e que ordena eles e deixa em orde crescente
+
 #include <stdio.h>
 
 int main() {
     int numeros[5];
     int i, j, temp;
 
-    printf("Digite 5 números inteiros:\n");
+    printf("Digite 5 numeros inteiros:\n");
     for (i = 0; i < 5; i++) {
-        printf("Digite o %dº número: ", i + 1);
+        printf("Digite o %dÂº numero: ", i + 1);
         scanf("%d", &numeros[i]);
     }
 
     for (i = 0; i < 5 - 1; i++) {
         for (j = 0; j < 5 - i - 1; j++) {
             if (numeros[j] > numeros[j + 1]) {
-                // Troca os números de lugar
                 temp = numeros[j];
                 numeros[j] = numeros[j + 1];
                 numeros[j + 1] = temp;
@@ -21,7 +22,7 @@ int main() {
         }
     }
 
-    printf("\nOs números em ordem crescente são:\n");
+    printf("\nOs numeros em ordem crescente sao:\n");
     for (i = 0; i < 5; i++) {
         printf("%d ", numeros[i]);
     }
