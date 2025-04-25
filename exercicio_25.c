f5d70737f65a0c5a2ad791ec27ffee6b0c703bd9
guizeuvirs0919 <guilherme.reis3@estudante.ifms.edu.br>
2025-04-24 21:44:06 -0400

    Update exercicio_25.c

--- exercicio_25.c
+++ exercicio_25.c
@@ -1,16 +1,18 @@
+// algoritimo que fala pro usuario escolher 6 numeros de um sorteio
+
 #include <stdio.h>
 
 int main() {
     int numeros[6];
 
-    printf("Digite 6 números para o sorteio:\n");
+    printf("Digite 6 nÃºmeros para o sorteio:\n");
 
     for (int i = 0; i < 6; i++) {
-        printf("Número %d: ", i + 1);
+        printf("NÃºmero %d: ", i + 1);
         scanf("%d", &numeros[i]);
     }
 
-    printf("\nNúmeros escolhidos para o sorteio:\n");
+    printf("\nNÃºmeros escolhidos para o sorteio:\n");
     for (int i = 0; i < 6; i++) {
         printf("%d ", numeros[i]);
     }
